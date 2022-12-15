ce48f596942e2afa776adfb9f7541aa825ef1938
eliashz <eliashz@gmail.com>
2022-12-15 20:07:30 +0100

    CardFull ok

--- src/components/molecules/CardFull.jsx
+++ src/components/molecules/CardFull.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import React from 'react'
-import { FlexBox } from '../../styles'
-import { Card } from '../atoms'
+import { Card, Text } from '../atoms'
 
 const Img = styled.img`
   border-radius: 5px;
@@ -9,11 +8,26 @@ const Img = styled.img`
   width: 200px;
   object-fit: cover;
 `
+
+const StyledCard = styled(Card)`
+  position: relative;
+  display: inline-block;
+  text-align: start;
+`
+const StyledTitle = styled(Text)`
+  position: absolute;
+  top: 150px;
+  left: 10px;
+`
+
 function CardFull() {
   return (
-    <Card>
+    <StyledCard>
+      <StyledTitle color="#fff" fontSize=".9em">
+        Piso en Barcelona, Grac... 939.000€
+      </StyledTitle>
       <Img src="../project/casa.jpeg" alt="Casa" />
-    </Card>
+    </StyledCard>
   )
 }
 
