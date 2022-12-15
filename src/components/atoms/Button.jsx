b09116bd8e5bcea58b54ac4847276dd5801e8f7c
eliashz <eliashz@gmail.com>
2022-12-15 18:11:25 +0100

    img

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -6,10 +6,26 @@ export default function Button({
   children,
   color = colors.main,
   backgroundColor = colors.base,
+  borderRadius,
   onClick = () => {},
 }) {
   return (
-    <button type="button" onClick={onClick} style={{ backgroundColor, color }}>
+    <button
+      type="button"
+      onClick={onClick}
+      style={{
+        backgroundColor,
+        color,
+        borderRadius,
+        fontSize: '.6em',
+        height: '25px',
+        border: 'none',
+        cursor: 'pointer',
+        padding: '0 15px 0 15px',
+        textAlign: 'center',
+        margin: '0 0 5px 0',
+      }}
+    >
       {children}
     </button>
   )
@@ -19,5 +35,6 @@ Button.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.string,
   backgroundColor: PropTypes.string,
+  borderRadiuss: PropTypes.string,
   onClick: PropTypes.func,
 }
--- src/components/molecules/CardHouse.jsx
+++ src/components/molecules/CardHouse.jsx
@@ -3,29 +3,35 @@ import { FlexBox } from '../../styles'
 import { Button, Card, Text, Title } from '../atoms'
 
 const Img = styled.img`
-  border-radius: '5px';
+  border-radius: 5px;
 `
 
 const TextImg = styled(Text)`
-  margin-left: '5px';
+  margin-left: 5px;
+  margin-right: 5x;
 `
 
 const ButtonImg = styled(Button)`
-  border-radius: '10px';
+  border-radius: 10px;
 `
-
 function CardHouse() {
   return (
     <Card>
       <FlexBox>
         <Img src="../project/casa.jpeg" alt="Casa" />
-        <TextImg>Piso en Barcelona, Gracia.</TextImg>
-        <FlexBox direction="row" justify="space-around">
+        <TextImg color="grey" fontSize=".8em">
+          Piso en Barcelona, Gracia
+        </TextImg>
+        <FlexBox direction="row" justify="space-around" align="center">
           <Title color="#000" fontSize=".9em">
             939.000€
           </Title>
-          <ButtonImg backgroundColor="steelblue">
-            <Text color="#fff">Localizar</Text>
+          <ButtonImg
+            backgroundColor="steelblue"
+            borderRadius="10px"
+            color="#fff"
+          >
+            Localizar
           </ButtonImg>
         </FlexBox>
       </FlexBox>
