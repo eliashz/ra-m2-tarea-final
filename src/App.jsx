abf618cbac7ad40749499e601253f6895b3b36e8
eliashz <eliashz@gmail.com>
2022-12-15 10:58:03 +0100

    map

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { Text } from './components/atoms'
+import Header from './components/layout/Header'
+import SubHeader from './components/layout/SubHeader'
 import { Body } from './components/layout'
 
 function App() {
   return (
-    <Body>
-      <Text as="h1">Hola a todos</Text>
-      <Text>Buenas tardes</Text>
-      <ul>
-        <Text as="li">A</Text>
-        <Text as="li">B</Text>
-        <Text as="li">C</Text>
-      </ul>
-    </Body>
+    <>
+      <Header />
+      <SubHeader />
+      <Body />
+    </>
   )
 }
 
--- src/components/layout/Body.jsx
+++ src/components/layout/Body.jsx
@@ -1,20 +1,35 @@
 import PropTypes from 'prop-types'
-import Header from './Header'
-import SubHeader from './SubHeader'
-import { FlexBox } from '../../styles'
+import styled from 'styled-components'
+
+import { colors, FlexBox } from '../../styles'
+
+const StyledDiv = styled.div`
+  background-color: ${colors.secondary};
+  border: 1px solid ${colors.tertiary};
+  width: 50%;
+  text-align: center;
+  margin: 2em;
+`
 
 function Body({ children }) {
   return (
-    <FlexBox>
-      <Header />
-      <SubHeader />
-      {children}
+    <FlexBox direction="row" justify="center">
+      <div
+        style={{
+          backgroundColor: 'lightgrey',
+          border: '1px solid grey',
+          width: '50%',
+        }}
+      >
+        Buscador
+      </div>
+      <StyledDiv>Mapa</StyledDiv>
     </FlexBox>
   )
 }
 
 Body.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 }
 
 export default Body
