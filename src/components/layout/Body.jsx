00ce34ae483f20659a8cbf6fedc8ed595f8e8101
eliashz <eliashz@gmail.com>
2022-12-15 16:22:52 +0100

    grid marchando

--- src/components/layout/Body.jsx
+++ src/components/layout/Body.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { colors, FlexBox, Grid } from '../../styles'
+import { colors, Grid } from '../../styles'
 import { Card } from '../molecules'
 
 const StyledDiv = styled.div`
   background-color: ${colors.secondary};
   border: 1px solid ${colors.tertiary};
-  width: 100%;
-  margin-right: 2em;
+
   text-align: center;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
@@ -15,12 +14,8 @@ const StyledDiv = styled.div`
 
 function Body({ children }) {
   return (
-    <Grid>
-      <Grid>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+    <Grid margin="2em">
+      <Grid columns="repeat(auto-fit, minmax(10rem, 1fr))">
         <Card />
         <Card />
         <Card />
--- src/styles/Grid.js
+++ src/styles/Grid.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 const Grid = styled.div`
     display: grid;
-    gap: 1em;
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-    margin: 2em 0 2em 2em;
+    grid-gap: 1em;
+    grid-template-columns: ${({columns}) => columns || '1fr 1fr'};
+    margin: ${({ margin }) => margin || '0'};
 `
 
 export default Grid
