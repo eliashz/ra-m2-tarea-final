aae8d4de998de2531a481163c28712ec7659fd3e
eliashz <eliashz@gmail.com>
2022-12-17 17:52:46 +0100

    cargar más button

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,11 +1,32 @@
+import styled from 'styled-components'
 import React from 'react'
 import { Body, SubHeader } from '../components/layout'
+import { FlexBox } from '../styles'
+import { Button } from '../components/atoms'
 
 function FrontPage() {
+  const StyledFlexBox = styled(FlexBox)`
+    width: 50%;
+    text-align: center;
+  `
   return (
     <>
       <SubHeader />
       <Body />
+      <StyledFlexBox justify="center" direction="row">
+        <Button
+          color="#fff"
+          backgroundColor="#4281e3"
+          borderRadius="7px"
+          fontSize=".7em"
+          height="25px"
+          width="120px"
+          border="none"
+          cursor="pointer"
+        >
+          Cargar más
+        </Button>
+      </StyledFlexBox>
     </>
   )
 }
