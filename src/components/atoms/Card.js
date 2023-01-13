566872d1fe1d93a8a5dda080feeacbc6cd152ea3
eliashz <eliashz@gmail.com>
2023-01-13 18:01:43 +0100

    Card

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components'
+import {card} from '../../styles'
 
 const Card = styled.div`
-  height: 11em;
-  width: 10em;
-  border-radius: 5px;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  ${card}
 `
 export default Card
